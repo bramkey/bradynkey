89b92419eab4260d6ef52fcd71145f5becfc65d8
bradyn <novemberfirebk@yahoo.com>
2025-04-08 23:33:03 -0400

    s

---
+++ main.js
@@ -0,0 +1,10 @@
+/* rotating popup folder text */
+
+function myFunction() {
+    var div = document.getElementById("myCODE");
+      if (div.style.display === "none") {
+        div.style.display = "block";
+      } else {
+        div.style.display = "none";
+      }
+    }
